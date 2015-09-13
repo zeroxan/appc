e2a717b9d20e640a4563f41a86e32cb57f0d54e7
bluemixtraining1 <bluemixtraining1@gmail.com>
2015-09-13 06:58:41 -0700

    added code to call service a and service b

--- src/main/java/appc/ServiceC.java
+++ src/main/java/appc/ServiceC.java
@@ -3,6 +3,12 @@ package appc;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
+import javax.xml.ws.BindingProvider;
+
+import appa.servicea.IServiceA;
+import appa.servicea.ServiceAService;
+import appb.serviceb.IServiceB;
+import appb.serviceb.ServiceBService;
 
 @Stateless
 @LocalBean
@@ -12,7 +18,15 @@ public class ServiceC implements IServiceC {
 	@Override
 	public int jumble(int leftOne, int rightOne, int leftTwo, int rightTwo) {
 		// TODO Use Service A & B to do this		
-		return (leftOne + rightOne) - (leftTwo + rightTwo);
+		
+		IServiceA serviceA = new ServiceAService().getServiceAPort();
+		((BindingProvider)serviceA).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:9080/appa/ServiceAService");
+		
+		IServiceB serviceB = new ServiceBService().getServiceBPort();
+		((BindingProvider)serviceB).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:9080/appb/ServiceBService");
+
+		return serviceB.minus(serviceA.plus(leftOne, rightOne),serviceA.plus(leftTwo, rightTwo));
+		
 	}
 
 }
